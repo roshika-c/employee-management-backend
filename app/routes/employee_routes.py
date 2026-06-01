e10a87505cdc2fb851c1b5368d2d216e8a62b375
roshika-c <roshikachandru2002@gmail.com>
2026-06-01 12:28:51 +0530

    updated service layer

--- app/routes/employee_routes.py
+++ app/routes/employee_routes.py
@@ -1,40 +1,25 @@
 from fastapi import APIRouter
 from models.employee_models import Employee
+from services import employee_services
 router=APIRouter()
-employees=[]
+
 @router.post("/add_employee")
 def add_employee(employee:Employee):
-    for emp in employees:
-        if emp["id"]==employee.id:
-            return {"message":"Employee already exists"}
-    employees.append(employee.model_dump())
-    return {"message":"Employee added successfully"}
+    return employee_services.add_employee(employee)
 
 @router.get("/all_employees")
 def get_all_employees():
-    return {"employees":employees}
+    return employee_services.get_all_employees()
 
 @router.get("/get_emp_by_id/{id}")
 def get_employee_by_id(id:int):
-    for employee in employees:
-        if employee["id"]==id:
-            return employee
-    return {"message":"Employee not found"}
+    return employee_services.get_employee_by_id(id)
 
 @router.put("/update_employees/{id}")
 def update_emp_by_id(updated_employee:Employee,id:int):
-    for index,employee in enumerate(employees):
-        if employee["id"]==id:
-            employees[index]=updated_employee.model_dump()
-            return {"message":"Employee updated successfully"}
-    return {"message":"Employee not found"}
-
+    return employee_services.update_emp_by_id(updated_employee,id)
 @router.delete("/delete_employees_by_id/{id}")
 def delete_emp_by_id(id:int):
-    for employee in employees:
-        if employee["id"]==id:
-            employees.remove(employee)
-            return {"message":"Employee deleted successfully"}
-    return {"message":"Employee not found"}
+    return employee_services.delete_emp_by_id(id)
 
 
