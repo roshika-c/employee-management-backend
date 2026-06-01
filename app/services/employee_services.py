be86c4dcfa051d56ee51270c2569f77cc21a87d4
roshika-c <roshikachandru2002@gmail.com>
2026-06-01 19:41:26 +0530

    logging updated

--- app/services/employee_services.py
+++ app/services/employee_services.py
@@ -1,29 +1,39 @@
+from utils.logger import logger
 from exceptions.employee_exceptions import EmployeeNotFoundException,EmployeeAlreadyExistsException
 employees=[]
 def add_employee(employee):
     for emp in employees:
         if emp["id"]==employee.id:
-            raise EmployeeAlreadyExistsException
+            logger.warning(f"Employee with {employee.id} already exists")
+            raise EmployeeAlreadyExistsException()
     employees.append(employee.model_dump())
+    logger.info(f"Employee with {employee.id} added successfully")
     return {"message":"Employee added successfully"}
 
 def get_all_employees():
+    logger.info("Employees found")
     return {"employees":employees}
 
 def get_employee_by_id(id):
     for employee in employees:
         if employee["id"]==id:
+            logger.info(f"Employee with {id} found")
             return employee
+    logger.error(f"Employee with {id} not found")
     raise EmployeeNotFoundException()
 def update_emp_by_id(updated_employee,id):
     for index,employee in enumerate(employees):
         if employee["id"]==id:
             employees[index]=updated_employee.model_dump()
+            logger.info(f"Employee with {id} updated successfully")
             return {"message":"Employee updated successfully"}
-    raise EmployeeNotFoundException
+    logger.error(f"Employee with {id} not found")
+    raise EmployeeNotFoundException()
 def delete_emp_by_id(id):
     for employee in employees:
         if employee["id"]==id:
             employees.remove(employee)
+            logger.info(f"Employee with id:{id} deleted successfully")
             return {"message":"Employee deleted successfully"}
-    raise EmployeeNotFoundException
+    logger.error(f"Employee with {id} not found")
+    raise EmployeeNotFoundException()
